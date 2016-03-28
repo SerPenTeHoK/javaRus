e42d941c99cfb40b6e8feb5be835d1128971059d
GSV <sergey-for-fotos@mail.ru>
2016-03-28 11:07:34 +0300

    .

--- src/java/main/test.java
+++
@@ -1,7 +0,0 @@
-package java.main;
-
-/**
- * Created by SerP on 28.03.2016.
- */
-public class test {
-}
---
+++ src/tests/test.java
@@ -0,0 +1,10 @@
+package tests;
+
+/**
+ * Created by SerP on 28.03.2016.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
